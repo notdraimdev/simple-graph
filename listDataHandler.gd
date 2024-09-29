f3b97c9bfb541316122afc1ff2eb1c337e1171be
notdraimdev <notdraimdev@gmail.com>
2024-09-29 11:06:30 +0200

    work on saving

--- listDataHandler.gd
+++ listDataHandler.gd
@@ -1,4 +1,4 @@
-extends Control
+extends Resource
 
 class ListModeData:
 	var colors:Dictionary
@@ -17,7 +17,14 @@ class ItemData:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var listdata:ListModeData = ListModeData.new()
+	var List = ListData.new()
+	listdata.lists.append(List)
+	var item = ItemData.new()
+	List.items.append(item)
+	var file = FileAccess.open("user://data.dat", FileAccess.WRITE)
+	ResourceSaver.save(List,"user://data.tres")
+	print(ResourceLoader.load("user://data.tres"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
